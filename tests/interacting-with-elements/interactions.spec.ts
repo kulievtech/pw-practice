d0d0f13cfc24af4481e8f2b2748af2a91ed2420b
Tohir Kuliev <kuliev.tech@gmail.com>
2025-12-06 15:39:57 -0500

    Update

--- tests/interacting-with-elements/interactions.spec.ts
+++ tests/interacting-with-elements/interactions.spec.ts
@@ -38,4 +38,25 @@ test("Checkboxes & Radio Buttons", async ({ page }) => {
 
 test("Dropdowns", async ({ page }) => {
   await page.goto("https://selectors-practice.onrender.com/");
+
+  // scroll to Dropdown section
+  await page.locator("#dropdownMenu").scrollIntoViewIfNeeded();
+
+  // Select Blue from Dropdown
+  await page.selectOption("#dropdownMenu", "Blue");
+
+  // Wait for 2 seconds to observe the change
+  await page.waitForTimeout(2000);
+
+  // Select Red from Dropdown
+  await page.selectOption("#dropdownMenu", "Red");
+
+  // Wait for 2 seconds to observe the change
+  await page.waitForTimeout(2000);
+
+  // Select Green from Dropdown
+  await page.selectOption("#dropdownMenu", "Green");
+
+  // Wait for 2 seconds to observe the change
+  await page.waitForTimeout(2000);
 });
