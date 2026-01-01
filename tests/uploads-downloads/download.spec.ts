23993bec1b9af9150a98dabcd3bf79e17eaa18c3
Tohir Kuliev <kuliev.tech@gmail.com>
2026-01-01 18:50:41 -0500

    update

--- tests/uploads-downloads/download.spec.ts
+++ tests/uploads-downloads/download.spec.ts
@@ -1,17 +1,41 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, Download } from "@playwright/test";
 
-test("Download a file", async ({ page }) => {
-  await page.goto("<https://example.com/download>");
+test("Download a single file", async ({ page }) => {
+  await page.goto("https://selectors-practice.onrender.com/");
 
   // Listen for download
   const [download] = await Promise.all([
     page.waitForEvent("download"), // Wait for download to start
-    page.click("#downloadButton"), // Click triggers download
+    page.click("#singleDownloadBtn"), // Click triggers download
   ]);
 
   // Save downloaded file
   await download.saveAs("downloads/myFile.txt");
 
-  // Log filename
-  console.log("Downloaded filename:", download.suggestedFilename());
+  // Verify download
+  await expect(page.locator("#singleDownloadOutput")).toHaveText(
+    "Single file download initiated!"
+  );
+});
+
+test("Download multiple files", async ({ page }) => {
+  await page.goto("https://selectors-practice.onrender.com/");
+
+  await page.click("#multiDownloadBtn");
+
+  // Collect downloads in an array
+  const downloads: Download[] = [];
+  for (let i = 0; i < 3; i++) {
+    downloads.push(await page.waitForEvent("download"));
+  }
+
+  // Save them
+  await Promise.all(
+    downloads.map((download, i) => download.saveAs(`downloads/file${i + 1}.txt`))
+  );
+
+  // Verify UI text
+  await expect(page.locator("#multiDownloadOutput")).toHaveText(
+    "Three files downloaded!"
+  );
 });
