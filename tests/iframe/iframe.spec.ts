1ff87118dc824f26adf5730965321eff56485d00
Tohir Kuliev <kuliev.tech@gmail.com>
2026-01-01 20:09:15 -0500

    Update

---
+++ tests/iframe/iframe.spec.ts
@@ -0,0 +1,45 @@
+import { test, expect } from "@playwright/test";
+
+// This test is skipped because it demonstrates that to interact with iframes you cannot use page.locator
+// Instead, you should use frameLocator as shown in the second test
+test.skip("Interact with iframe using page", async ({ page }) => {
+  await page.goto("https://selectors-practice.onrender.com/");
+
+  // Create a frame locator
+  const iframe = page.locator("#practiceIframe");
+
+  // Click button inside iframe
+  await iframe.locator("#iframeBtn").click();
+
+  // Assert
+  await expect(iframe.locator("#iframeOutput")).toHaveText(
+    "Button inside iframe clicked!"
+  );
+
+  // Fill input inside iframe
+  await iframe.locator("#iframeInput").fill("Hello iFrame");
+
+  // Assert
+  await expect(iframe.locator("#iframeInput")).toHaveValue("Hello iFrame");
+});
+
+test("Interact with iframe using frameLocator", async ({ page }) => {
+  await page.goto("https://selectors-practice.onrender.com/");
+
+  // Create a frame locator
+  const iframe = page.frameLocator("#practiceIframe");
+
+  // Click button inside iframe
+  await iframe.locator("#iframeBtn").click();
+
+  // Assert
+  await expect(iframe.locator("#iframeOutput")).toHaveText(
+    "Button inside iframe clicked!"
+  );
+
+  // Fill input inside iframe
+  await iframe.locator("#iframeInput").fill("Hello iFrame");
+
+  // Assert
+  await expect(iframe.locator("#iframeInput")).toHaveValue("Hello iFrame");
+});
